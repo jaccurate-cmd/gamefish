52345d5ec81f537d214f06a9343545659e3e0780
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-26 12:15:27 -0400

    Add Vector3.Separate that assumes normal

--- Project/gamefish/Libraries/fish.kit-base/Code/Util/Extensions/Vector3.cs
+++ Project/gamefish/Libraries/fish.kit-base/Code/Util/Extensions/Vector3.cs
@@ -15,13 +15,22 @@ public static partial class GameFish
 	/// </summary>
 	/// <param name="v"></param>
 	/// <param name="dir"> The forward-facing direction. </param>
-	/// <param name="fwd"> Velocity pointing in that direction. </param>
-	/// <param name="side"> Relative horizontal velocity. </param>
+	/// <param name="fwd"> Vector pointing in that direction. </param>
+	/// <param name="side"> Relative horizontal vector. </param>
 	public static void Separate( this Vector3 v, in Vector3 dir, out Vector3 fwd, out Vector3 side )
 	{
 		side = v.SubtractDirection( dir );
 		fwd = v - side;
 	}
+
+	/// <summary>
+	/// Separate a vector into its forward/sideways components using its normal(direction).
+	/// </summary>
+	/// <param name="v"></param>
+	/// <param name="fwd"> Vector pointing in that direction. </param>
+	/// <param name="side"> Relative horizontal vector. </param>
+	public static void Separate( this Vector3 v, out Vector3 fwd, out Vector3 side )
+		=> v.Separate( v.Normal, out fwd, out side );
 
 	/// <summary>
 	/// Gets the forward component of a vector using the specified direction. <br />
@@ -31,10 +40,7 @@ public static partial class GameFish
 	/// <param name="dir"> The forward-facing direction. </param>
 	/// <returns> The forward component of the vector using the specified direction. </returns>
 	public static Vector3 Forward( this Vector3 v, in Vector3 dir )
-	{
-		// Log( "Vector3.Forward", v - v.SubtractDirection( dir ) );
-		return v - v.SubtractDirection( dir );
-	}
+		=> v - v.SubtractDirection( dir );
 
 	/// <summary>
 	/// Gets the sideways component of a vector using the specified direction. <br />
@@ -44,10 +50,7 @@ public static partial class GameFish
 	/// <param name="dir"> The sideways-facing direction. </param>
 	/// <returns> The sideways component of the vector using the specified direction. </returns>
 	public static Vector3 Sideways( this Vector3 v, in Vector3 dir )
-	{
-		// Log( "Vector3.Sideways", v.SubtractDirection( dir ) );
-		return v.SubtractDirection( dir );
-	}
+		=> v.SubtractDirection( dir );
 
 	/// <summary>
 	/// <see cref="Vector3.Clamp(Vector3,Vector3)"/>
