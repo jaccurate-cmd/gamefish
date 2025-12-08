0c5912ce3d99c02ecb4a3af5eadbb84bf8737338
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-08 11:51:20 -0500

    FacepunchController uses built-in eye rotation

--- Project/gamefish/Libraries/fish.kit-base/Code/Systems/Agency/Controllers/Engine/FacepunchController.cs
+++ Project/gamefish/Libraries/fish.kit-base/Code/Systems/Agency/Controllers/Engine/FacepunchController.cs
@@ -24,11 +24,10 @@ public partial class FacepunchController : BaseController
 		if ( !PlayerController.IsValid() )
 		{
 			this.Warn( "needs a PlayerController to function!" );
+			return;
 		}
-		else
-		{
-			PlayerController.UseCameraControls = false;
-		}
+
+		PlayerController.UseCameraControls = false;
 	}
 
 	protected override void OnUpdate()
@@ -39,20 +38,28 @@ public partial class FacepunchController : BaseController
 			PlayerController.UseInputControls = Pawn.IsValid() && Pawn.AllowInput();
 	}
 
-	public override Vector3 GetLocalEyePosition()
-	{
-		if ( PlayerController.IsValid() )
-			return WorldTransform.PointToLocal( PlayerController.EyePosition );
-
-		return base.GetLocalEyePosition();
-	}
-
 	public override void SetLocalEyePosition( Vector3 pos ) { }
 
 	protected override void OnSetLocalEyeRotation( in Rotation r )
 	{
 		if ( PlayerController.IsValid() )
 			PlayerController.EyeAngles = r;
+	}
+
+	public override Rotation GetLocalEyeRotation()
+	{
+		if ( PlayerController.IsValid() )
+			return PlayerController.EyeAngles;
+
+		return base.GetLocalEyeRotation();
+	}
+
+	public override Vector3 GetLocalEyePosition()
+	{
+		if ( PlayerController.IsValid() )
+			return WorldTransform.PointToLocal( PlayerController.EyePosition );
+
+		return base.GetLocalEyePosition();
 	}
 
 
