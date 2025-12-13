4e3ec82de5392a6bdbf9619b7b4093a107213ad5
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-12 20:55:20 -0500

    Global stringssssssss

--- Project/gamefish/Libraries/fish.gamefish-kit/Code/Util/Library/Strings.cs
+++ Project/gamefish/Libraries/fish.gamefish-kit/Code/Util/Library/Strings.cs
@@ -3,22 +3,31 @@ namespace GameFish;
 public static partial class Strings
 {
 	public const string LIBRARY = "📚 Library";
+	public const string LIBRARIES = "📚 Libraries";
 
 	public const string DEBUG = "🐞 Debug";
-	public const string DEBUGGING = DEBUG + "ging";
+	public const string DEBUGGER = "🐞 Debugger";
+	public const string DEBUGGING = "🐞 Debugging";
 
 	public const string BOOT = "👢 Boot";
 	public const string BOOTING = BOOT + "ing";
 
-	public const string GAME = "🎮 Game";
-	public const string GAMES = GAME + "s";
-	public const string SERVER = "💻 Server";
-	public const string SERVERS = SERVER + "s";
 	public const string SESSION = "📋 Session";
-	public const string SESSIONS = SESSION + "s";
+	public const string SESSIONS = "📋 Sessions";
+
+	public const string SERVER = "💻 Server";
+	public const string SERVERS = "💻 Servers";
 
 	public const string CHEATS = "👨‍💻 Cheats";
 	public const string CHEATING = "👨‍💻 Cheating";
+
+	public const string GAME = "🎮 Game";
+	public const string GAMES = "🎮 Games";
+
+	public const string STATE = "🍰 State";
+	public const string STATES = "🍰 States";
+	public const string MODE = "🎛️ Mode";
+	public const string MODES = "🎛️ Modes";
 
 	public const string ID = "🆔 Identity";
 	public const string IDS = "🆔 Identities";
@@ -26,9 +35,13 @@ public static partial class Strings
 	public const string TAG = "🔖 Tag";
 	public const string TAGS = "🔖 Tags";
 	public const string TAGGING = "🔖 Tagging";
+	public const string FILTER = "📋 Filter";
+	public const string FILTERS = "📋 Filters";
+	public const string ATTRIBUTE = "🔧 Attribute";
+	public const string ATTRIBUTES = "🔧 Attributes";
 
 	public const string NOTE = "📝 Note";
-	public const string NOTES = NOTE + "s";
+	public const string NOTES = "📝 Notes";
 
 	public const string PREFAB = "🔵 Prefab";
 	public const string PREFABS = "🔵 Prefabs";
@@ -37,76 +50,50 @@ public static partial class Strings
 	public const string ENTITIES = "📦 Entities";
 
 	public const string TRIGGER = "📡 Trigger";
-	public const string TRIGGERS = TRIGGER + "s";
+	public const string TRIGGERS = "📡 Triggers";
 
-	public const string INPUT = "🕹 Input";
 	public const string USE = "🖱 Use";
+	public const string INPUT = "🕹 Input";
 
 	public const string NETWORK = "📶 Network";
 	public const string NETWORKING = "📶 Networking";
 
 	public const string MODULE = "🧩 Module";
-	public const string MODULES = MODULE + "s";
+	public const string MODULES = "🧩 Modules";
 
 	public const string CALLBACK = "⚡ Callback";
-	public const string CALLBACKS = CALLBACK + "s";
+	public const string CALLBACKS = "⚡ Callbacks";
 
-	public const string ATTRIBUTES = "🔧 Attributes";
-	public const string FILTERS = "📋 Filters";
 	public const string LOGIC = "🧠 Logic";
-
-	public const string STATE = "🍰 State";
-	public const string STATES = STATE + "s";
-	public const string MODE = "🎛️ Mode";
-	public const string MODES = MODE + "s";
+	public const string LOGICAL = "🧠 Logical";
+	public const string MATH = "🤓 Math";
+	public const string MATHS = "🤓 Maths";
+	public const string COUNT = "🧮 Count";
+	public const string COUNTS = "🧮 Counts";
+	public const string COUNTING = "🧮 Counting";
 
 	public const string VIEW = "🎥 View";
-	public const string VIEWING = VIEW + "ing";
+	public const string VIEWS = "🎥 Views";
+	public const string VIEWING = "🎥 Viewing";
 	public const string CYCLE = "♻ Cycle";
+	public const string CYCLES = "♻ Cycles";
 	public const string CYCLING = "♻ Cycling";
 	public const string TRANSITION = "↔ Transition";
-	public const string TRANSITIONING = TRANSITION + "ing";
+	public const string TRANSITIONS = "↔ Transitions";
+	public const string TRANSITIONING = "↔ Transitioning";
 
 	public const string UI = "📺 UI";
+	public const string TV = "📺 TV";
+	public const string TVS = "📺 TVs";
 	public const string DISPLAY = "📛 Display";
 
-	public const string ART = "🖼 Art";
-	public const string PAINT = "🖌 Paint";
-	public const string PAINTING = PAINT + "ing";
-	public const string THEME = "🎨 Theme";
-	public const string THEMES = THEME + "s";
-
-	public const string MUTE = "🔇 Mute";
-	public const string MUTED = MUTE + "d";
-	public const string MUTING = "🔇 Muting";
-	public const string AUDIO = "🔊 Audio";
-	public const string AUDIBLE = "🔊 Audible";
-
-	public const string MIXER = "🎛 Mixer";
-	public const string MIXERS = MIXER + "s";
-	public const string VOLUME = "🎚 Volume";
-	public const string VOLUMES = VOLUME + "s";
-
-	public const string RADIO = "📻 Radio";
-	public const string MUSIC = "🎵 Music";
-	public const string TRACK = "🎼 Track";
-	public const string TRACKS = TRACK + "s";
-	public const string SOUNDTRACK = "🎹 Soundtrack";
-	public const string SOUNDTRACKS = SOUNDTRACK + "s";
-
-	public const string MIC = "🎙 Mic";
-	public const string MICS = MIC + "s";
-	public const string MICROPHONE = "🎙 Microphone";
-	public const string MICROPHONES = MICROPHONE + "s";
-	public const string HEADPHONES = "🎧 Headphones";
-
 	public const string FILE = "💾 File";
-	public const string FILES = FILE + "s";
+	public const string FILES = "💾 Files";
 	public const string SAVE = "💿 Save";
-	public const string SAVES = SAVE + "s";
+	public const string SAVES = "💿 Saves";
 	public const string SAVING = "💿 Saving";
 	public const string COMPRESS = "🗜 Compress";
-	public const string COMPRESSION = COMPRESS + "ion";
+	public const string COMPRESSION = "🗜 Compression";
 
 	public const string CONFIG = "⚙ Config";
 	public const string SETTINGS = "⚙ Settings";
@@ -128,43 +115,70 @@ public static partial class Strings
 	public const string DIRECTION = "🧭 Direction";
 	public const string DIRECTIONS = "🧭 Directions";
 
-	public const string DEPTH = "👇 Depth";
-	public const string DEPTHS = DEPTH + "s";
-
+	public const string CORDON = "🚧 Cordon";
+	public const string CORDONS = "🚧 Cordons";
 	public const string BARRIER = "🚧 Barrier";
-	public const string BARRIERS = BARRIER + "s";
+	public const string BARRIERS = "🚧 Barriers";
 
 	public const string BOUNDS = "📐 Bounds";
 	public const string BOUNDARY = "📐 Boundary";
 	public const string BOUNDARIES = "📐 Boundaries";
 
+	public const string DEPTH = "👇 Depth";
+	public const string DEPTHS = "👇 Depths";
+
 	public const string TELEPORT = "🌌 Teleport";
-	public const string TELEPORTING = TELEPORT + "ing";
-	public const string TELEPORTATION = TELEPORT + "ation";
+	public const string TELEPORTING = "🌌 Teleporting";
+	public const string TELEPORTATION = "🌌 Teleportation";
+
+	public const string MUTE = "🔇 Mute";
+	public const string MUTED = "🔇 Muted";
+	public const string MUTING = "🔇 Muting";
+	public const string AUDIO = "🔊 Audio";
+	public const string AUDIBLE = "🔊 Audible";
+
+	public const string MIXER = "🎛 Mixer";
+	public const string MIXERS = "🎛 Mixers";
+	public const string VOLUME = "🎚 Volume";
+	public const string VOLUMES = "🎚 Volumes";
+
+	public const string RADIO = "📻 Radio";
+	public const string RADIOS = "📻 Radios";
+	public const string MUSIC = "🎵 Music";
+	public const string TRACK = "🎼 Track";
+	public const string TRACKS = "🎼 Tracks";
+	public const string SOUNDTRACK = "🎹 Soundtrack";
+	public const string SOUNDTRACKS = "🎹 Soundtracks";
+
+	public const string MIC = "🎙 Mic";
+	public const string MICS = "🎙 Mics";
+	public const string MICROPHONE = "🎙 Microphone";
+	public const string MICROPHONES = "🎙 Microphones";
+	public const string HEADPHONES = "🎧 Headphones";
 
 	public const string SPECTATOR = "👻 Spectator";
-	public const string SPECTATORS = SPECTATOR + "s";
+	public const string SPECTATORS = "👻 Spectators";
 	public const string SPECTATING = "👻 Spectating";
 
 	public const string AGENT = "👨‍💼 Agent";
-	public const string AGENTS = AGENT + "s";
+	public const string AGENTS = "👨‍💼 Agents";
 	public const string CLIENT = "👨‍💼 Client";
-	public const string CLIENTS = CLIENT + "s";
+	public const string CLIENTS = "👨‍💼 Clients";
 	public const string PAWN = "🐴 Pawn";
-	public const string PAWNS = PAWN + "s";
+	public const string PAWNS = "🐴 Pawns";
 
 	public const string PLAYER = "😎 Player";
-	public const string PLAYERS = PLAYER + "s";
+	public const string PLAYERS = "😎 Players";
 
 	public const string ENEMY = "😡 Enemy";
 	public const string ENEMIES = "😡 Enemies";
 
 	public const string NPC = "🤖 NPC";
-	public const string NPCS = NPC + "s";
+	public const string NPCS = "🤖 NPCs";
 	public const string ACTOR = "🎭 Actor";
-	public const string ACTORS = ACTOR + "s";
+	public const string ACTORS = "🎭 Actors";
 	public const string MIND = "🧠 Mind";
-	public const string MINDS = MIND + "s";
+	public const string MINDS = "🧠 Minds";
 
 	public const string DETECTION = "🔍 Detection";
 	public const string HEARING = "👂 Hearing";
@@ -176,11 +190,11 @@ public static partial class Strings
 	public const string FLYING = "🦅 Flying";
 
 	public const string CONSTRAINT = "🧲 Constraint";
-	public const string CONSTRAINTS = CONSTRAINT + "s";
+	public const string CONSTRAINTS = "🧲 Constraints";
 	public const string PHYSICS = "🍎 Physics";
 	public const string MOMENTUM = "➡ Momentum";
 	public const string FORCE = "💨 Force";
-	public const string FORCES = FORCE + "s";
+	public const string FORCES = "💨 Forces";
 	public const string DRAG = "🐌 Drag";
 
 	public const string COLLISION = "⚽ Collision";
@@ -190,50 +204,74 @@ public static partial class Strings
 	public const string BOUNCING = "🏀 Bouncing";
 
 	public const string EFFECT = "🎇 Effect";
-	public const string EFFECTS = EFFECT + "s";
+	public const string EFFECTS = "🎇 Effects";
 	public const string SOUND = "🔊 Sound";
-	public const string SOUNDS = SOUND + "s";
-
-	public const string CARD = "🃏 Card";
-	public const string CARDS = CARD + "s";
+	public const string SOUNDS = "🔊 Sounds";
 
 	public const string LUCK = "🍀 Luck";
+	public const string LUCKINESS = "🍀 Luckiness";
+	public const string CARD = "🃏 Card";
+	public const string CARDS = "🃏 Cards";
 	public const string WEIGHT = "⚖ Weight";
+	public const string WEIGHTS = "⚖ Weights";
 	public const string WEIGHING = "⚖ Weighing";
+	public const string GAMBLE = "🎰 Gamble";
+	public const string GAMBLING = "🎰 Gambling";
 	public const string CHANCE = "🎲 Chance";
+	public const string CHANCES = "🎲 Chances";
 	public const string RANDOM = "🎲 Random";
-	public const string RANDOMNESS = RANDOM + "ness";
-	public const string RANDOMIZATION = RANDOM + "ization";
+	public const string RANDOMNESS = "🎲 Randomness";
+	public const string RANDOMIZATION = "🎲 Randomization";
 
 	public const string TEAM = "🚩 Team";
-	public const string TEAMS = TEAM + "s";
+	public const string TEAMS = "🚩 Teams";
 	public const string FACTION = "🚩 Faction";
-	public const string FACTIONS = FACTION + "s";
+	public const string FACTIONS = "🚩 Factions";
 
-	public const string ATM = "🏧 ATM";
+	public const string ATMS = "🏧 ATMs";
 	public const string BANK = "🏦 Bank";
+	public const string BANKS = "🏦 Banks";
 	public const string CASH = "💵 Cash";
 	public const string MONEY = "💰 Money";
+	public const string MONIES = "💰 Monies";
 	public const string VALUE = "💲 Value";
+	public const string VALUES = "💲 Values";
 
+	public const string UNLOCK = "🔐 Unlock";
 	public const string UNLOCKS = "🔐 Unlocks";
+	public const string TIME = "⏰ Time";
+	public const string TIMES = "⏰ Times";
 	public const string TIMING = "⏰ Timing";
 
-	public const string HEALTH = "💖 Health";
-	public const string LIVES = "💖 Lives";
 	public const string LIFE = "💖 Life";
+	public const string LIVES = "💖 Lives";
+	public const string HEALTH = "💖 Health";
 	public const string DEATH = "💀 Death";
+	public const string DEATHS = "💀 Deaths";
 
-	public const string MODEL = "💠 Model";
-	public const string MODELS = MODEL + "s";
+	public const string ART = "🖼 Art";
+	public const string PAINT = "🖌 Paint";
+	public const string PAINTS = "🖌 Paints";
+	public const string PAINTING = "🖌 Painting";
+	public const string COLOR = "🎨 Color";
+	public const string COLORS = "🎨 Colors";
+	public const string THEME = "🎨 Theme";
+	public const string THEMES = "🎨 Themes";
+
+	public const string MODEL = "🗿 Model";
+	public const string MODELS = "🗿 Models";
 	public const string BODY = "🕺 Body";
 	public const string BODIES = "🕺 Bodies";
+	public const string AVATAR = "😊 Avatar";
+	public const string AVATARS = "😊 Avatars";
+	public const string OUTFIT = "👗 Outfit";
+	public const string OUTFITS = "👗 Outfits";
 
 	public const string AMMO = "🍟 Ammo";
 	public const string EQUIP = "🛠 Equip";
-	public const string EQUIPMENT = EQUIP + "ment";
+	public const string EQUIPMENT = "🛠 Equipment";
 	public const string WEAPON = "🔫 Weapon";
-	public const string WEAPONS = WEAPON + "s";
+	public const string WEAPONS = "🔫 Weapons";
 	public const string INVENTORY = "🎒 Inventory";
 
 	public const string AIM = "👓 Aim";
@@ -241,7 +279,8 @@ public static partial class Strings
 	public const string COMBAT = "⚔ Combat";
 
 	public const string CHARGE = "🔋 Charge";
-	public const string CHARGER = CHARGE + "r";
+	public const string CHARGES = "🔋 Charges";
+	public const string CHARGER = "🔋 Charger";
 	public const string CHARGING = "🔋 Charging";
 
 	public const string RESERVE = "🧰 Reserve";
@@ -280,14 +319,14 @@ public static partial class Strings
 	public const string UNLOCKING = "🔑 Unlocking";
 
 	public const string DOOR = "🚪 Door";
-	public const string DOORS = DOOR + "s";
+	public const string DOORS = "🚪 Doors";
 	public const string ROOM = "🏛 Room";
-	public const string ROOMS = ROOM + "s";
+	public const string ROOMS = "🏛 Rooms";
 	public const string CHAMBER = "🔳 Chamber";
-	public const string CHAMBERS = CHAMBER + "s";
+	public const string CHAMBERS = "🔳 Chambers";
 
 	public const string ANCHOR = "⚓ Anchor";
-	public const string ANCHORS = ANCHOR + "s";
+	public const string ANCHORS = "⚓ Anchors";
 
 	public const string TAG_TRIGGER = "trigger";
 	public const string TAG_LADDER = "ladder";
