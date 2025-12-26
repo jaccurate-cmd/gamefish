0ef9f8c7524f3a7eee3c0f6f6b03405820aa5e03
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-26 17:12:54 -0500

    SimpleVehicle is configurable, a bit more polished

--- Project/gamefish/Libraries/fish.gamefish-kit/Code/Systems/Agency/Driving/Vehicle/SimpleVehicle.cs
+++ Project/gamefish/Libraries/fish.gamefish-kit/Code/Systems/Agency/Driving/Vehicle/SimpleVehicle.cs
@@ -1,7 +1,40 @@
 namespace GameFish;
 
+/// <summary>
+/// A stupidly simple vehicle that doesn't care about wheels.
+/// </summary>
 public partial class SimpleVehicle : Vehicle
 {
+	/// <summary>
+	/// How much to push the vehicle forward from acceleration input.
+	/// </summary>
+	[Property]
+	[Feature( VEHICLE ), Order( VEHICLE_ORDER )]
+	public float DrivingSpeed { get; set; } = 800f;
+
+	/// <summary>
+	/// The relative direction to push the vehicle forward/back.
+	/// </summary>
+	[Normal]
+	[Property]
+	[Feature( VEHICLE ), Order( VEHICLE_ORDER )]
+	public Vector3 DrivingAxis { get; set; } = Vector3.Forward;
+
+	/// <summary>
+	/// How much to spin the vehicle from steering input.
+	/// </summary>
+	[Property]
+	[Feature( VEHICLE ), Order( VEHICLE_ORDER )]
+	public float SteeringTorque { get; set; } = 6f;
+
+	/// <summary>
+	/// The relative direction to spin the vehicle around.
+	/// </summary>
+	[Normal]
+	[Property]
+	[Feature( VEHICLE ), Order( VEHICLE_ORDER )]
+	public Vector3 SteeringAxis { get; set; } = Vector3.Up;
+
 	protected override void ApplyForces( in float deltaTime, in bool isFixedUpdate )
 	{
 		if ( IsProxy || !Rigidbody.IsValid() )
@@ -10,9 +43,19 @@ public partial class SimpleVehicle : Vehicle
 		var rDrive = WorldRotation;
 
 		if ( InputAcceleration != 0f )
-			Velocity += rDrive.Forward * InputAcceleration * 1000f * deltaTime;
+		{
+			var drivingAxis = rDrive * DrivingAxis;
+			var speed = InputAcceleration * DrivingSpeed;
+
+			Velocity += rDrive.Forward * speed * deltaTime;
+		}
 
 		if ( InputSteering != 0f )
-			Rigidbody.AngularVelocity += rDrive.Up * InputSteering * 5f * deltaTime;
+		{
+			var steeringAxis = rDrive * SteeringAxis;
+			var torque = InputSteering * SteeringTorque;
+
+			Rigidbody.AngularVelocity += steeringAxis * torque * deltaTime;
+		}
 	}
 }
