6424f7908e4a4fc2b0947dc6c2f041484b2b444c
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-27 15:55:38 -0500

    EditorEntity defaults host owner, allows takeover

--- Project/gamefish/code/Playground/Editing/Entities/Base/EditorEntity.Net.cs
+++ Project/gamefish/code/Playground/Editing/Entities/Base/EditorEntity.Net.cs
@@ -6,6 +6,8 @@ partial class EditorEntity
 	protected override bool IsNetworkedAutomatically => true;
 
 	protected override NetworkMode NetworkingModeDefault => NetworkMode.Object;
-	protected override OwnerTransfer NetworkTransferModeDefault => OwnerTransfer.Fixed;
+	protected override OwnerTransfer NetworkTransferModeDefault => OwnerTransfer.Takeover;
 	protected override NetworkOrphaned NetworkOrphanedModeDefault => NetworkOrphaned.Host;
+
+	public override Connection DefaultNetworkOwner => Connection.Host;
 }
